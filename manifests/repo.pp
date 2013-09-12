5e7dcc8f36c83020938a9712ff5d38f02f04137d
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2013-09-12 15:50:08 +0200

    switch to Puppet Labs apt module

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,8 +3,8 @@
 == Class: qgis::repo
 
 Configure apt-get to fetch all qgis packages from qgis.org.
-You can still prefer install your distribution's packages by setting
-another apt::preferences resource with a higher priority.
+You can still prefer install your distribution packages by setting
+another apt::pin resource with a higher priority.
 
 Only compatible with debian/ubuntu.
 
@@ -13,20 +13,39 @@ class qgis::repo {
 
   if $::osfamily == 'Debian' {
 
-    apt::sources_list { 'qgis.org':
-      ensure  => present,
-      content => "deb http://qgis.org/debian ${::lsbdistcodename} main",
+    apt::source {'qgis-org':
+      ensure      => present,
+      location    => 'http://qgis.org/debian',
+      release     => $::lsbdistcodename,
+      repos       => 'main',
+      include_src => false,
     }
 
     apt::key {'47765B75':
       ensure    => present,
     }
 
-    apt::preferences {'qgis':
-      ensure   => present,
-      package  => 'libqgis-dev libqgis1 python-qgis-common python-qgis qgis-api-doc qgis-common qgis-mapserver qgis-plugin-grass-common qgis-plugin-grass qgis-providers-common qgis-providers qgis-sqlanywhere* qgis',
-      pin      => 'release o=Quantum GIS project',
-      priority => 1100,
+    $qgis_packages = [
+      'libqgis-dev',
+      'libqgis1',
+      'python-qgis-common',
+      'python-qgis',
+      'qgis-api-doc',
+      'qgis-common',
+      'qgis-mapserver',
+      'qgis-plugin-grass-common',
+      'qgis-plugin-grass',
+      'qgis-providers-common',
+      'qgis-providers',
+      'qgis-sqlanywhere*',
+      'qgis',
+    ]
+
+    apt::pin {'qgis-org':
+      ensure     => present,
+      packages   => inline_template('<%= qgis_packages.join(" ") %>'),
+      originator => 'Quantum GIS project',
+      priority   => 1100,
     }
   }
 
