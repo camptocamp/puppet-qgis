36a2fc61716d3b040d5e6a1b8aa0cd53740205e6
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-26 17:04:45 +0100

    Add unit tests

---
+++ spec/classes/qgis__desktop_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'qgis::desktop' do
+
+  let(:pre_condition) do
+    "include ::apt"
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+    end
+  end
+end
